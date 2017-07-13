c6cad89688e255a4e9a478237003590a4baf4d8e
Hugh Bethell <hbethell@outlook.com>
2017-07-13 15:15:54 -0700

    Merge fixes

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,42 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-     var pointsArray = document.getElementsByClassName('point');
- 
-     var revealpoints = function(points) {
-		 for (var i =0; i< points.length; i++) {
-         	points[i].style.opacity = 1;
-         	points[i].style.transform = "scaleX(1) translateY(0)";
-         	points[i].style.msTransform = "scaleX(1) translateY(0)";
-         	points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-		 };
-     };
-
-window.onload = function () {
-	
-	 /* Automatically animate the points on a tall screen where scrolling can't trigger the animation */
-	if (window.innerHeight > 950) {
-		revealpoints(pointsArray);
-		}
-	
-	var sellingPoints = document.getElementsByClassName("selling-points")[0];
-	var scrollDistance = sellingPoints.getBoundingClientRect().top-window.innerHeight+200;
-	
-	window.addEventListener("scroll", function(event) {
-		console.log("Current offset from the top is" +sellingPoints.getBoundingClientRect().top+ " pixels");
-		if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-			revealpoints(pointsArray);
-		}
-	})
-}
-=======
-var animatePoints = function() {
-      var points = document.getElementsByClassName('point');
-=======
 var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
       
->>>>>>> checkpoint-9
   
       var revealPoint = function(index) {
 		 
@@ -50,9 +15,6 @@ var animatePoints = function(points) {
      revealPoint(i);
 		}
  };
-<<<<<<< HEAD
->>>>>>> checkpoint-8-assignment
-=======
 
 window.onload = function() {
 	
@@ -70,4 +32,3 @@ window.onload = function() {
          }
 	});
 }
->>>>>>> checkpoint-9
