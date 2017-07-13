644ca663fe616c16cc8df3a6bd5a485c6cf27851
Hugh Bethell <hbethell@outlook.com>
2017-07-13 10:27:04 -0700

    checkpoint 8 assignment revisions

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,10 @@
-var animatePoints = function() {
- 
      var points = document.getElementsByClassName('point');
  
      var revealpoints = function() {
-		 for (var i =0; i<3; i++) {
+		 for (var i =0; i< points.length; i++) {
          	points[i].style.opacity = 1;
          	points[i].style.transform = "scaleX(1) translateY(0)";
          	points[i].style.msTransform = "scaleX(1) translateY(0)";
          	points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
 		 };
      };
-     revealpoints();
- };
