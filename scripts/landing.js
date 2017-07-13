2f3790532f3fdc24a6489cbd428805aa6e460177
Hugh Bethell <hbethell@outlook.com>
2017-07-13 12:31:49 -0700

    checkpoint 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-     var points = document.getElementsByClassName('point');
+     var pointsArray = document.getElementsByClassName('point');
  
-     var revealpoints = function() {
+     var revealpoints = function(points) {
 		 for (var i =0; i< points.length; i++) {
          	points[i].style.opacity = 1;
          	points[i].style.transform = "scaleX(1) translateY(0)";
@@ -8,3 +8,21 @@
          	points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
 		 };
      };
+
+window.onload = function () {
+	
+	 /* Automatically animate the points on a tall screen where scrolling can't trigger the animation */
+	if (window.innerHeight > 950) {
+		revealpoints(pointsArray);
+		}
+	
+	var sellingPoints = document.getElementsByClassName("selling-points")[0];
+	var scrollDistance = sellingPoints.getBoundingClientRect().top-window.innerHeight+200;
+	
+	window.addEventListener("scroll", function(event) {
+		console.log("Current offset from the top is" +sellingPoints.getBoundingClientRect().top+ " pixels");
+		if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+			revealpoints(pointsArray);
+		}
+	})
+}
