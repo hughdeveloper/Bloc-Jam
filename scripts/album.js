489bfa3c2e4aff3593e2e615fa357a9539a9bb5c
Hugh Bethell <hbethell@outlook.com>
2017-07-21 12:11:42 -0700

    checkpoint 13

--- scripts/album.js
+++ scripts/album.js
@@ -66,19 +66,89 @@ var songListContainer = document.getElementsByClassName('album-view-song-list')[
 var songRows = document.getElementsByClassName('album-view-song-item');
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtomTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+var currentlyPlayingSong = null;
 
  window.onload = function() {
      setCurrentAlbum(albumMarconi);
 	 
 	 songListContainer.addEventListener('mouseover', function(event) {
 		if (event.target.parentElement.className === 'album-view-song-item') {
-			event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-		 }
+			var songItem = getSongItem(event.target);
+			if (songItem.getAttribute('data-song-number') != currentlyPlayingSong) {
+				songItem.innerHTML = playButtonTemplate;
+			}
+		}
 	 });
+	 
+	 
+	 var findParentByClassName = function (name, targetname) {
+		 if (name) {
+			 var parent = name.parentElement;
+			 while (parent.className !== targetname && parent.className !== null) {
+				 parent = parent.parentElement;
+			 }
+			 return parent;
+		 }
+		 
+	 };
+	 
+	 // switch rather than if else statements. Don't need ot use break becasue when you return break is automatically assumed
+	 
+	 var getSongItem = function(element) {
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }  
+};
+	 
+	 var clickHandler = function(targetElement) {
+		 var songItem = getSongItem(targetElement);
+		 
+		 // if there is no song playing
+		 if (currentlyPlayingSong === null) {
+			 songItem.innerHTML = pauseButtomTemplate;
+			 currentlyPlayingSong = songItem.getAttribute("data-song-number");
+		 }
+		 // if there is a song playing and you click on the same song to pause it
+		 else if (currentlyPlayingSong === songItem.getAttribute("data-song-number")){
+			 songItem.innerHTML = playButtonTemplate;
+			 currentlyPlayingSong = null;
+		 }
+		 //if you click on a different song to play something else
+		 else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+         	var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+         	currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+         	songItem.innerHTML = pauseButtomTemplate;
+         	currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     	}
+	 };
+	 
 	 
 	 for (var i = 0; i < songRows.length; i++) {
          songRows[i].addEventListener('mouseleave', function(event) {
-             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+             var songItem = getSongItem(event.target);
+			 var songItemNumber = songItem.getAttribute('data-song-number');
+			  if (songItemNumber != currentlyPlayingSong) {
+				  songItem.innerHTML = songItemNumber;
+			  }
          });
+		 
+		songRows[i].addEventListener('click', function(event) {
+			clickHandler(event.target);
+		});
+		 
      }
  };
