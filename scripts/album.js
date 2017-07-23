41b91e5150bc9aff459eaa408ab93fdcb31cfe1d
Hugh Bethell <hbethell@outlook.com>
2017-07-23 12:09:26 -0700

    comments

--- scripts/album.js
+++ scripts/album.js
@@ -1,4 +1,4 @@
- 
+ // the albums arrays. usually this information will be pulled from a database
 //we have created an object//
 var albumPicasso = {
      title: 'The Colors',
@@ -43,13 +43,18 @@ var createSongRow = function(songNumber, songName, songLength) {
  };
 
 var setCurrentAlbum = function(album) {
-
+	//slects  the album title
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
+	//selects the album's artist element
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+	// selects the ablum information element
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+	//selects the image element
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
+	//selects the album's list of music. (the whole list)
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  
+	/*goes and looks at the first child of the album title element then it grabs the nodevalue (the information within the element) of that element and sets it equal to the album.title. It statement displays the title of the element*/
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -57,7 +62,9 @@ var setCurrentAlbum = function(album) {
  
      albumSongList.innerHTML = "";
 
+	/*this for loop inputs the information form the album songs into there places starting with the first song and increasing until the list of songs have all gone through the loop*/
      for (var i = 0; i < album.songs.length; i++) {
+		 // template estbalishes first number than the song title and then the song duration
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
@@ -68,22 +75,11 @@ var songRows = document.getElementsByClassName('album-view-song-item');
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtomTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
+//setting the current playing song to none to begin. this is so a song does not automatically start playing.
 var currentlyPlayingSong = null;
 
- window.onload = function() {
-     setCurrentAlbum(albumMarconi);
-	 
-	 songListContainer.addEventListener('mouseover', function(event) {
-		if (event.target.parentElement.className === 'album-view-song-item') {
-			var songItem = getSongItem(event.target);
-			if (songItem.getAttribute('data-song-number') != currentlyPlayingSong) {
-				songItem.innerHTML = playButtonTemplate;
-			}
-		}
-	 });
-	 
-	 
-	 var findParentByClassName = function (name, targetname) {
+/*finding the parent name. going form parent to parent if the targetname is not found and if there is no more parent elements that the while loop will stop looping*/
+var findParentByClassName = function (name, targetname) {
 		 if (name) {
 			 var parent = name.parentElement;
 			 while (parent.className !== targetname && parent.className !== null) {
@@ -91,7 +87,6 @@ var currentlyPlayingSong = null;
 			 }
 			 return parent;
 		 }
-		 
 	 };
 	 
 	 // switch rather than if else statements. Don't need ot use break becasue when you return break is automatically assumed
@@ -135,8 +130,23 @@ var currentlyPlayingSong = null;
          	currentlyPlayingSong = songItem.getAttribute('data-song-number');
      	}
 	 };
+
+ window.onload = function() {
+     setCurrentAlbum(albumMarconi);
 	 
+	 /*This function states that when the mouse is over a certain row that the playbuttom icon will be displayed. The condition that controls this function will only show the play buttom icon if the song is not a curently playing song.*/
+	 songListContainer.addEventListener('mouseover', function(event) {
+		if (event.target.parentElement.className === 'album-view-song-item') {
+			/* getSongItem(event.target) finds the element that holds the number*/
+			//console.log(getSongItem(event.target));
+			var songItem = getSongItem(event.target);
+			if (songItem.getAttribute('data-song-number') != currentlyPlayingSong) {
+				songItem.innerHTML = playButtonTemplate;
+			}
+		}
+	 });
 	 
+	 /*once the mouse leaves the certain row the function will exicute and not display the play buttom but rather display the number. The condition that allows for this will only run if the song is not playing*/
 	 for (var i = 0; i < songRows.length; i++) {
          songRows[i].addEventListener('mouseleave', function(event) {
              var songItem = getSongItem(event.target);
