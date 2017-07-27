8fb3e5adf662a3937c93e4886dab99299ade63c7
Hugh Bethell <hbethell@outlook.com>
2017-07-27 13:35:37 -0700

    checkpoint 16

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,4 +1,4 @@
-var pointsArray = document.getElementsByClassName('point');
+/*var pointsArray = document.getElementsByClassName('point');
 
  var revealPoint = function(point) {
 		 
@@ -10,21 +10,49 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {  
 	forEach(points, revealPoint);
- };
+ }; */
 
-window.onload = function() {
+
+var animatePoints = function() {
+	var revealPoint = function() {
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+		 };
+		$.each($('.point'), revealPoint);
+	};
+
+
+//window.onload = function() {
+
+$(window).load(function() {
 	
-	if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-     }
+	     // Automatically animates the points on a tall screen where scrolling can't trigger the animation		Old code
+     /*if (window.innerHeight > 950) {
+         animatePoints(pointsArray);*/
+		 
+	//jQuery version of the code
+	if($(window).height() > 950) {
+		animatePoints();
+	}
 
 	
-	var sellingPoints = document.getElementsByClassName('selling-points')[0];
-	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight +200;
+	/*var sellingPoints = document.getElementsByClassName('selling-points')[0];
+	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight +200;*/
 	
-	window.addEventListener('scroll', function(event) {
+	var scrollDistance = $('.selling-points').offset().top-$(window).height()+200;
+	
+	
+	
+	/*window.addEventListener('scroll', function(event) {
 		 if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
-         }
+             animatePoints(pointsArray);  */
+	
+	$(window).scroll(function(event) {
+		if ($(window).scrollTop() >= scrollDistance) {
+			animatePoints();
+		}
 	});
-}
+//}
+});
