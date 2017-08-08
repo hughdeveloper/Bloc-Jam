4f1f0e2871c7fc550105ba114d0ab5ed5450a053
Hugh Bethell <hbethell@outlook.com>
2017-08-08 15:19:12 -0700

    checkpoint 21

--- scripts/album.js
+++ scripts/album.js
@@ -1,6 +1,51 @@
 /* When coding you do not want to pollute the global atmosphere. It can lead to problems when coding with other poeple. This can be solved by making the whole document a in a function. There are other ways of solving this problem. */
 
 //template for song rows//
+
+var updateSeekPercentage = function($seekBar, seekBarFillRatio) {
+    var offsetXPercent = seekBarFillRatio * 100;
+    
+    offsetXPercent = Math.max(0, offsetXPercent);
+    offsetXPercent = Math.min(100, offsetXPercent);
+ 
+    
+    var percentageString = offsetXPercent + '%';
+    $seekBar.find('.fill').width(percentageString);
+    $seekBar.find('.thumb').css({left: percentageString});
+ };
+
+var setupSeekBars = function() {
+     var $seekBars = $('.player-bar .seek-bar');
+ 
+     $seekBars.click(function(event) {
+         
+         var offsetX = event.pageX - $(this).offset().left;
+         var barWidth = $(this).width();
+         
+         var seekBarFillRatio = offsetX / barWidth;
+ 
+         updateSeekPercentage($(this), seekBarFillRatio);
+     });
+	
+	$seekBars.find('.thumb').mousedown(function(event) {
+
+         var $seekBar = $(this).parent();
+
+         $(document).bind('mousemove.thumb', function(event){
+             var offsetX = event.pageX - $seekBar.offset().left;
+             var barWidth = $seekBar.width();
+             var seekBarFillRatio = offsetX / barWidth;
+ 
+             updateSeekPercentage($seekBar, seekBarFillRatio);
+         });
+
+         $(document).bind('mouseup.thumb', function() {
+             $(document).unbind('mousemove.thumb');
+             $(document).unbind('mouseup.thumb');
+         });
+     });
+ };
+
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -253,17 +298,7 @@ var togglePlayFromPlayerBar = function () {
 		 }
 	 };
 
-var updateSeekPercentage = function($seekBar, seekBarFillRatio) {
-	var offsetXPercent = seekBarFillRatio*100;
-	
-	offsetXPercent = Math.max(0, offsetXPercent);
-	offsetXPercent = Math.min(100, offsetXPercent);
-	
-	var percentageString = offsetXPercent + '%';
-	$seekBar.find('.fill').wifth(percentageString);
-	$seekBar.find('.thumb').css({left: percentageString});
-};
-
+ 
 
 
 //var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
@@ -294,6 +329,7 @@ var $nextButton = $('.main-controls .next');
 // is document nessary here ????? can you go with "$(function() {"
  $(document).ready(function () {
 	 setCurrentAlbum(albumPicasso);
+	 setupSeekBars();
 	 //when we click on the previous button we will run the fuction previousSong
 	 $previousButton.click(previousSong);
 	 //when we click on the next button we will run the function nextSong
